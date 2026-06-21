e8b49d4bb8ad3624830e742567b09e009b20e5a2
Rabia Arif <rabiaarif694@gmail.com>
2026-06-21 21:18:58 +0500

    chnages

--- python-practice-sheet/problem4.py
+++ python-practice-sheet/problem4.py
@@ -1,9 +1,20 @@
 # count even and odd using list comprehension
-list = []
-for i in range(5):
-    input_list = int(input("Enter the number:"))
-    list.append(input_list)
-    
-list_input = [j%2==0 for j in len(list)]
+list = [2,3,13,12,14,16,11,10]
+# for i in range(5):
+#     input_list = int(input("Enter the number:"))
+#     list.append(input_list)
+
+list_input = ["even"  if x % 2 == 0 else "odd" for x in list]
 print(list_input)
 # print(list)
+even = 0
+odd = 0
+for j in list_input:
+    # print(j)
+    if j == 'even':
+        even+=1
+    else:
+        odd+=1
+
+
+print(f"output : Even:{even} , Odd:{odd}")
