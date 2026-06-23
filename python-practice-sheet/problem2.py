9eaaef38b17bdab50c3ac7fd9036f53210901c0f
Rabia Arif <rabiaarif694@gmail.com>
2026-06-23 09:55:35 +0500

    student management

---
+++ python-practice-sheet/problem2.py
@@ -0,0 +1,27 @@
+# Student Management system
+
+def accept(name: str,roll_no: str,marks1: int,marks2: int):
+ 
+    return name,roll_no,marks1,marks2
+    
+    
+
+
+
+# student1=accept("aimen","23ai01",83,79)
+
+def display(name,roll_no,marks1,marks2):
+    print("Display Student Information")
+    name =name
+    roll_no = roll_no
+    marks1 = marks1
+    marks2 = marks2
+    print(f"Name: {name}")
+    print(f"Roll Number:{roll_no}")
+    print(f"Subject 1:{marks1}")
+    print(f"Subject 2:{marks2}")
+    
+
+print(dis =display(accept("aimen","23ai01",83,79)))
+
+############# not complete ###########
