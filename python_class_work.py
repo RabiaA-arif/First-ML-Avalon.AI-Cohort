706d464dbc4d3bd487109e8889f165805a3cd0fc
Rabia Arif <rabiaarif694@gmail.com>
2026-07-06 22:31:12 +0500

    module in python

--- python_class_work.py
+++ python_class_work.py
@@ -1,1 +1,24 @@
-  
+###### MOdule in python
+# 1 built in module
+# 2 user defined module
+# 3 External module
+
+############## importing method
+# 1 simple import 
+# import scikit_learn
+
+# 2 import with alis 
+# import scikit_learn as sk
+
+# 3 import for specific
+
+from pandas import DataFrame
+
+##########################################################
+
+# operating management system
+import os
+
+print(os.cpu_count())
+print(os.listdir())
+# print(os.fdopen())
