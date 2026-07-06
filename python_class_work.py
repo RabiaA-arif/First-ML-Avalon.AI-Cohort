9ddbb62874eb8c74cdbf9ec6424ace3faa8fa25a
Rabia Arif <rabiaarif694@gmail.com>
2026-07-06 22:32:58 +0500

    python class work

--- python_class_work.py
+++ python_class_work.py
@@ -22,3 +22,31 @@ import os
 print(os.cpu_count())
 print(os.listdir())
 # print(os.fdopen())
+
+
+
+
+def resturant_bill(oder):
+  rate = 0
+  if oder == "birayani":
+    print(f"rate is 300")
+
+  elif oder == "qourma":
+    print("rate is 1000")
+
+  else:
+    print("we have not that item in menu ")
+
+
+obj = resturant_bill("birayani")
+print(obj)
+
+
+
+
+### Lottery system
+
+import random
+for i in range(5):
+  random_num = random.randint(100,200)
+  print(f"Random number is:{random_num}")
